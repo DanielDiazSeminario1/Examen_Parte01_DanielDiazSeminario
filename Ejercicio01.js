f696f61ed77f6c0231a762f5e789dfec9f790020
DanielDiaz <you@example.com>
2022-05-11 11:06:01 -0500

    Ejercicio01-listo

---
+++ Ejercicio01.js
@@ -0,0 +1,7 @@
+//Ingrese datos en
+
+let n = 6;
+
+let sum = (n + 3) / (n + 4) - 0.75;
+
+console.log(sum);
---
+++ Ejercicio02.js
@@ -0,0 +1,9 @@
+//Ingrese datos en num
+let num = 25500;
+let numtext = num.toString();
+
+let arr = numtext.split("");
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
